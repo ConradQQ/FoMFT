925399e7cd359c6e01e62747c73abcb4ef98a495
conradqq <connorhawkesps4@gmail.com>
2025-03-17 13:46:03 -0500

    Outlined electron app

--- preload.js
+++
@@ -1,6 +0,0 @@
-// preload.ts (or preload.js)
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getEnv: () => ipcRenderer.sendSync('get-env'),
-});
