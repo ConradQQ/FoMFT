645e22e8f23d8b91cb4b8b924f7e87aa32297579
conradqq <connorhawkesps4@gmail.com>
2025-03-18 08:24:33 -0500

    modified component logic

--- src/ui/components/ItemMenu.jsx
+++ src/ui/components/ItemMenu.jsx
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ItemMenu = ({onItemSelect}) => {
+    // State variables
   const [categories, setCategories] = useState([
     'armor',
     'weapons',
+    'implants',
     'food',
     'boosters',
-    'implants',
     'meds',
   ]);
+  const [armorTypes, setArmorTypes] = ([
+    'helmet',
+    'torso',
+    'arms',
+    'legs',
+    'shoulders'
+  ]);
 
+  const [selectedArmorType, setSelectedArmorType] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+ 
 
+  // API Call based on selected category
   useEffect(() => {
     if(selectedCategory) {
       const fetchData = async () => {
@@ -32,9 +43,31 @@ const ItemMenu = ({onItemSelect}) => {
       fetchData();
     }
     
-  }, [selectedCategory])
+  }, [selectedCategory]);
+
+  // API Call specifically for armor that handles armor types
+
+  useEffect(() => {
+    if(selectedCategory && selectedArmorType) {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(`http://localhost:3000/${selectedCategory}/${selectedArmorType}`);
+          setItems(response.data);
+          setLoading(false);
+        } catch (err) {
+          setError(err.message || 'Error Fetching Items');
+          setLoading(false);
+        }
+      };
+      fetchData();
+    }
+    
+  }, [selectedCategory, selectedArmorType]);
 
   const handleCategoryClick = (category) => {
+    // Removes lingering error message after failed click
+    if (error !== null) setError(null);
     setSelectedCategory(category);
   };
   
@@ -62,8 +95,8 @@ const ItemMenu = ({onItemSelect}) => {
           {items.length > 0 && (
             <ul className='item-list overflow-y-auto max-h-100'>
               {items.map((item) => (
-                <li key={item.id || item.armor_id || item.material_id} onClick={() => handleItemClick(item)}>
-                  {item.name || item.armor_name || item.material_name}
+                <li key={item.id || item.armor_id || item.weapon_id || item.food_id || item.implant_id || item.med_id} onClick={() => handleItemClick(item)}>
+                  {item.name || item.armor_name || item.weapon_name || item.food_name || item.implant_name || item.med_id}
                 </li>
               ))}
             </ul>
