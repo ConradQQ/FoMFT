d62fe127bd2baf1ab9882f60052411e5075bb246
conradqq <connorhawkesps4@gmail.com>
2025-03-20 14:22:29 -0500

    Added more business logic for stats tracker

--- src/ui/components/StatsDisplay.jsx
+++ src/ui/components/StatsDisplay.jsx
@@ -1,22 +1,32 @@
 const StatsDisplay = ({ itemObjects, slotTypes }) => {
   const aggregatedStats = {};
 
-  
+  itemObjects.forEach(item => {
+    if (item.stats) {
+      for (const key in item.stats) {
+        const value = item.stats[key];
+        if (aggregatedStats[key]) {
+          // If the key exists, add the value to the existing value
+          aggregatedStats[key] += value;
+        } else {
+          // If the key doesn't exist, initialize it as a single value
+          aggregatedStats[key] = value;
+        }
+      }
+    }
+  });
 
+
+
+ 
   return (
     <div className="stats-display mt-4 p-4 bg-amber-500 rounded-md">
       <h2>Total Stats</h2>
-      {itemObjects.map((item, index) => {
-        console.log(itemObjects)
-        return Object.entries(item.stats).map(([key, value]) => {
-          console.log("Key:", key, "Value:", value); // Added console.log
-          return (
-            <p key={index + "-" + key}>
-              {key}: {value}
-            </p>
-          );
-        });
-      })}
+      {Object.entries(aggregatedStats).map(([statName, statValue]) => (
+        <p key={statName}>
+          {statName}: {statValue}
+        </p>
+      ))}
     </div>
   );
 };
