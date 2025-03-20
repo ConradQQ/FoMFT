9e2294a20cbc06c808226f5f1b3355ba5ac6de90
conradqq <connorhawkesps4@gmail.com>
2025-03-20 17:32:29 -0500

    Seperated stats display into Defensive and Offensive stat displays

--- src/ui/components/StatsDisplay.jsx
+++ src/ui/components/StatsDisplay.jsx
@@ -1,34 +1,75 @@
-const StatsDisplay = ({ itemObjects, slotTypes }) => {
+const StatsDisplay = ({ itemObjects }) => {
+
   const aggregatedStats = {};
+  const armorStats = {};
+  const weaponStats = {};
 
   itemObjects.forEach(item => {
     if (item.stats) {
       for (const key in item.stats) {
         const value = item.stats[key];
         if (aggregatedStats[key]) {
-          // If the key exists, add the value to the existing value
           aggregatedStats[key] += value;
         } else {
-          // If the key doesn't exist, initialize it as a single value
           aggregatedStats[key] = value;
         }
       }
     }
   });
+  // Logic for adding stats to armorStats object
+  itemObjects.forEach(item => {
+    if(Object.keys(item)[0] === 'armor_id') {
+      if (item.stats) {
+        for (const key in item.stats) {
+          const value = item.stats[key];
+          if (armorStats[key]) {
+            armorStats[key] += value;
+          } else {
+            armorStats[key] = value;
+          }
+        }
+      }
+  }});
+
+  // Logic for adding stats to weaponStats object
+  itemObjects.forEach(item => {
+    if(Object.keys(item)[0] === 'weapon_id') {
+      if (item.stats) {
+        for (const key in item.stats) {
+          const value = item.stats[key];
+          if (weaponStats[key]) {
+            weaponStats[key] += value;
+          } else {
+            weaponStats[key] = value;
+          }
+        }
+      }
+  }});
 
 
-
- 
   return (
-    <div className="stats-display mt-4 p-4 bg-amber-500 rounded-md">
-      <h2>Total Stats</h2>
-      {Object.entries(aggregatedStats).map(([statName, statValue]) => (
-        <p key={statName}>
-          {statName}: {statValue}
-        </p>
-      ))}
+    <div className="stats-container">
+      <div className="defensive-stats mt-4 p-4 bg-amber-500 rounded-md">
+        <h2 className="underline sm:text-sm">Defensive Stats</h2>
+        {Object.entries(armorStats).map(([statName, statValue]) => (
+          <p key={statName} className="sm:text-sm">
+            {statName}: {statValue}
+          </p>
+        ))}
+      </div>
+      <div className="offensive-stats mt-4 p-4 bg-amber-500 rounded-md">
+        <h2 className="underline sm:text-sm">Offensive Stats</h2>
+        {Object.entries(weaponStats).map(([statName, statValue]) => (
+          <p key={statName} className='sm:text-sm'>
+            {statName}: {statValue}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default StatsDisplay;
+
+
+ 
